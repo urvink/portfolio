1d1adc27811403f110ee190c150a989a889668b2
Urvin <urvinkirindongo@gmail.com>
2021-02-07 03:29:40 +0100

    Big - Update

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,21 @@
-let info;
-function getData(info){
-    fetch('/app/js/data/myInfo.json')
-    .then( response =>{
-        if(!response.ok){
-            throw new Error(`Fout!! ${response.status}`);
-        }else{
-            info = response.json();
-        }
-    });
-}
 
-getData(info);
-console.log(info);
+/**
+ * My Info details
+ */
+fetch('/app/js/data/myInfo.json')
+.then( response =>{
+    if(!response.ok){
+        throw new Error(`Fout!! ${response.status}`);
+    }else{
+        return response.json();
+    }
+})
+.then(data =>{
+    console.log(data.personalia.name);
+    document.querySelector(".lead").innerHTML = data.lead.NL;
+    document.querySelector("h1").textContent = data.personalia.name;
+
+});
 
 // fetch("./app/js/data/myInfo.json")
 // .then(response => response.json())
